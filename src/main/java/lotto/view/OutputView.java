9afd2f6479a863c26754d697125742f12f9f7ad7
longrunpc <pcjmo4050@gmail.com>
2025-11-03 19:51:46 +0900

    feat(OutputView): 출력 메시지 개선 및 상수화
    OutputView 클래스에서 구매 개수 출력 방식을 개선하여, 상수화된 메시지를 사용하도록 변경했습니다.

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -2,6 +2,7 @@ package lotto.view;
 
 import java.util.List;
 
+import lotto.common.message.OutputMessage;
 import lotto.dto.PurchasedLotto;
 
 public class OutputView {
@@ -11,9 +12,9 @@ public class OutputView {
     }
 
     private static void printPurchaseCount(int count) {
-        System.out.println(count + "개를 구매했습니다.");
+        System.out.println(OutputMessage.PURCHASE_COUNT.message().formatted(count));
     }
-    
+
     private static void printPurchasedLottos(List<PurchasedLotto> purchasedLottos) {
         for (PurchasedLotto purchasedLotto : purchasedLottos) {
             System.out.println(purchasedLotto.lottos());
