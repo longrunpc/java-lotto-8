41c9e9db5240d8595854a3207e4b8aea3c147806
longrunpc <pcjmo4050@gmail.com>
2025-11-03 16:07:35 +0900

    feat(WinningLotto): WinningLotto 클래스 추가
    로또 당첨 번호와 보너스 번호를 관리하는 WinningLotto 클래스를 추가했습니다.

---
+++ src/main/java/lotto/domain/winning/WinningLotto.java
@@ -0,0 +1,13 @@
+package lotto.domain.winning;
+
+import lotto.domain.lotto.Lotto;
+
+public class WinningLotto {
+    private final Lotto winningNumbers;
+    private final int bonusNumber;
+
+    public WinningLotto(Lotto winningNumbers, int bonusNumber) {
+        this.winningNumbers = winningNumbers;
+        this.bonusNumber = bonusNumber;
+    }
+}
