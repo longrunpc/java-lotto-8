8bec1772797fbbc79d166a3af2ee8c33a12eddbc
longrunpc <pcjmo4050@gmail.com>
2025-11-03 14:38:37 +0900

    feat(Budget): 예산 생성 시 유효성 검사 추가
    예산 생성 메서드에 금액 유효성 검사를 추가했습니다.
    - 금액이 0원 이상이어야 하며,
    - 1000원 단위로 입력해야 합니다.

--- src/main/java/lotto/domain/finance/Budget.java
+++ src/main/java/lotto/domain/finance/Budget.java
@@ -8,6 +8,17 @@ public class Budget {
     }
 
     public static Budget create(int amount) {
+        validate(amount);
         return new Budget(amount);
+    }
+
+    private static void validate(int amount) {
+        if (amount < 0) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 0원 이상이어야 합니다.");
+        }
+
+        if (amount % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 1000원 단위로 입력해야 합니다.");
+        }
     }
 }
