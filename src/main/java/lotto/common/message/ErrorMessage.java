34e9e39be87922da59089fe84be7d971bbc42bd6
longrunpc <pcjmo4050@gmail.com>
2025-11-03 14:46:47 +0900

    feat(ErrorMessage): 오류 메시지 열거형 추가
    구입 금액 관련 오류 메시지를 관리하기 위해 ErrorMessage 열거형을 추가하였습니다.
    - INVALID_PURCHASE_AMOUNT: 구입 금액은 0원 이상이어야 함
    - INVALID_PURCHASE_AMOUNT_UNIT: 구입 금액은 1000원 단위로 입력해야 함

---
+++ src/main/java/lotto/common/message/ErrorMessage.java
@@ -0,0 +1,16 @@
+package lotto.common.message;
+
+public enum ErrorMessage {
+    INVALID_PURCHASE_AMOUNT("[ERROR] 구입 금액은 0원 이상이어야 합니다."),
+    INVALID_PURCHASE_AMOUNT_UNIT("[ERROR] 구입 금액은 1000원 단위로 입력해야 합니다.");
+
+    private final String message;
+
+    ErrorMessage(String message) {
+        this.message = message;
+    }
+
+    public String message() {
+        return message;
+    }
+}
