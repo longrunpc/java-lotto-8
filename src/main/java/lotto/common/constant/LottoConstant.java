d1f221c366c0db20f836f2d7538e1d8ba8850629
longrunpc <pcjmo4050@gmail.com>
2025-11-03 15:12:09 +0900

    refactor(RandomLottoGenerator): 로또 상수 추가 및 랜덤 로또 생성 로직 수정
    LottoConstant에 로또 크기 및 번호 범위 상수 추가.
    RandomLottoGenerator에서 상수를 사용하여 번호 생성 로직 개선.

--- src/main/java/lotto/common/constant/LottoConstant.java
+++ src/main/java/lotto/common/constant/LottoConstant.java
@@ -3,4 +3,8 @@ package lotto.common.constant;
 public class LottoConstant {
     public static final int LOTTO_PRICE = 1000;
     public static final int MIN_PURCHASE_AMOUNT = 0;
+
+    public static final int LOTTO_SIZE = 6;
+    public static final int LOTTO_START_NUMBER = 0;
+    public static final int LOTTO_END_NUMBER = 45;
 }
--- src/main/java/lotto/domain/lotto/RandomLottoGenerator.java
+++ src/main/java/lotto/domain/lotto/RandomLottoGenerator.java
@@ -3,11 +3,12 @@ package lotto.domain.lotto;
 import java.util.List;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import lotto.common.constant.LottoConstant;
 
 public class RandomLottoGenerator implements LottoGenerator {
     @Override
     public Lotto generate() {
-        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(0, 45, 6);
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(LottoConstant.LOTTO_START_NUMBER, LottoConstant.LOTTO_END_NUMBER, LottoConstant.LOTTO_SIZE);
         return new Lotto(numbers);
     }
 }
