ddbbaa8e91034957c2b7430b91a2fd468de36a96
longrunpc <pcjmo4050@gmail.com>
2025-11-03 15:13:08 +0900

    feat(Lotto): 로또 번호 유효성 검사 메시지 추가
    ErrorMessage에 로또 번호 개수에 대한 오류 메시지를 추가하고, Lotto 클래스에서 해당 메시지를 사용하도록 수정하였습니다.

--- src/main/java/lotto/common/message/ErrorMessage.java
+++ src/main/java/lotto/common/message/ErrorMessage.java
@@ -2,7 +2,9 @@ package lotto.common.message;
 
 public enum ErrorMessage {
     INVALID_PURCHASE_AMOUNT("[ERROR] 구입 금액은 0원 이상이어야 합니다."),
-    INVALID_PURCHASE_AMOUNT_UNIT("[ERROR] 구입 금액은 1000원 단위로 입력해야 합니다.");
+    INVALID_PURCHASE_AMOUNT_UNIT("[ERROR] 구입 금액은 1000원 단위로 입력해야 합니다."),
+
+    INVALID_LOTTO_SIZE("[ERROR] 로또 번호는 6개여야 합니다.");
 
     private final String message;
 
