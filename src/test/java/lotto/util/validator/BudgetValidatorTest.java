98c173ef55cdf4c1a360551f12c80b90a43214f7
longrunpc <pcjmo4050@gmail.com>
2025-11-03 21:27:29 +0900

    test(BudgetValidator): BudgetValidator에 대한 단위 테스트 추가
    BudgetValidator의 유효성 검사를 위한 단위 테스트를 추가하였습니다.
    - 유효한 금액(0원 및 1000원 단위) 통과 테스트
    - 0원 미만 금액에 대한 예외 테스트
    - 1000원 단위가 아닌 금액에 대한 예외 테스트
    - 구입 개수가 Integer.MAX_VALUE를 초과할 경우 예외 테스트 추가.

---
+++ src/test/java/lotto/util/validator/BudgetValidatorTest.java
@@ -0,0 +1,51 @@
+package lotto.util.validator;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.math.BigDecimal;
+
+import static org.assertj.core.api.Assertions.assertThatNoException;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class BudgetValidatorTest {
+
+    @DisplayName("0원 및 1000원 단위 금액은 통과한다")
+    @ParameterizedTest
+    @ValueSource(strings = {"0", "1000", "5000", "123000"})
+    void should_pass_when_amount_is_valid_multiple_of_1000(String amount) {
+        assertThatNoException()
+            .isThrownBy(() -> BudgetValidator.validateBudget(new BigDecimal(amount)));
+    }
+
+    @DisplayName("0원 미만이면 예외")
+    @ParameterizedTest
+    @ValueSource(strings = {"-1", "-1000", "-999999"})
+    void should_throw_when_amount_below_minimum(String amount) {
+        assertThatThrownBy(() -> BudgetValidator.validateBudget(new BigDecimal(amount)))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("1000원 단위가 아니면 예외")
+    @ParameterizedTest
+    @ValueSource(strings = {"1", "999", "1500", "1001", "1234567"})
+    void should_throw_when_amount_not_multiple_of_1000(String amount) {
+        assertThatThrownBy(() -> BudgetValidator.validateBudget(new BigDecimal(amount)))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("구입 개수가 Integer.MAX_VALUE를 초과하면 예외")
+    @Test
+    void should_throw_when_lotto_count_exceeds_integer_max() {
+        BigDecimal lottoPrice = BigDecimal.valueOf(lotto.common.constant.LottoConstant.LOTTO_PRICE);
+        BigDecimal overMaxCount = BigDecimal.valueOf((long) Integer.MAX_VALUE).add(BigDecimal.ONE);
+        BigDecimal amount = lottoPrice.multiply(overMaxCount);
+
+        assertThatThrownBy(() -> BudgetValidator.validateBudget(amount))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+}
+
+
