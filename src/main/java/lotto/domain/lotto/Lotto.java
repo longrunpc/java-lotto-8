f4084016bc1e469d35467426a1ed97c1989c960e
longrunpc <pcjmo4050@gmail.com>
2025-11-03 16:18:35 +0900

    refactor(RandomLottoGenerator): 로또 번호 검증 로직을 RandomLottoGenerator로 이동
    Lotto 클래스에서 로또 번호 검증을 제거하고, RandomLottoGenerator에서 검증하도록 변경했습니다. 이를 통해 로또 번호 생성 시 유효성을 확보합니다.

--- src/main/java/lotto/domain/lotto/Lotto.java
+++ src/main/java/lotto/domain/lotto/Lotto.java
@@ -2,13 +2,10 @@ package lotto.domain.lotto;
 
 import java.util.List;
 
-import lotto.util.validator.LottoValidator;
-
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        LottoValidator.validateLottoNumbers(numbers);
         this.numbers = numbers;
     }
 }
--- src/main/java/lotto/domain/lotto/RandomLottoGenerator.java
+++ src/main/java/lotto/domain/lotto/RandomLottoGenerator.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import lotto.common.constant.LottoConstant;
+import lotto.util.validator.LottoValidator;
 
 public class RandomLottoGenerator implements LottoGenerator {
 
@@ -20,6 +21,7 @@ public class RandomLottoGenerator implements LottoGenerator {
     @Override
     public Lotto generate() {
         List<Integer> numbers = Randoms.pickUniqueNumbersInRange(LottoConstant.LOTTO_START_NUMBER, LottoConstant.LOTTO_END_NUMBER, LottoConstant.LOTTO_SIZE);
+        LottoValidator.validateLottoNumbers(numbers);
         return new Lotto(numbers);
     }
 }
