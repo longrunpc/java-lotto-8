a85be1664a9b3f4e9cb92ceddcbe49b52a7a4c51
longrunpc <pcjmo4050@gmail.com>
2025-11-03 19:50:08 +0900

    feat(OutputMessage): 출력 메시지 enum 추가
    OutputMessage enum을 추가하여 로또 구매 및 당첨 통계 관련 메시지를 정의하였습니다.

---
+++ src/main/java/lotto/common/message/OutputMessage.java
@@ -0,0 +1,20 @@
+package lotto.common.message;
+
+public enum OutputMessage {
+    PURCHASE_COUNT("%d개를 구매했습니다."),
+    WINNING_STATISTICS("당첨 통계"),
+    WINNING_STATISTICS_SEPARATOR("---"),
+    WINNING_REPORT_ENTRY("%d개 일치 (%d원) - %d개"),
+    WINNING_REPORT_ENTRY_WITH_BONUS("%d개 일치, 보너스 볼 일치 (%d원) - %d개"),
+    TOTAL_PROFIT_RATE("총 수익률은 %s입니다.");
+
+    private final String message;
+
+    OutputMessage(String message) {
+        this.message = message;
+    }
+
+    public String message() {
+        return message;
+    }
+}
